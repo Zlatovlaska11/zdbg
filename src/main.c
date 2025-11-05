1c4ef90a872df0d3f22bd1195f7a06befd5f8f14
Marian Pecqueur <marpecqueur@gmail.com>
2025-11-05 18:38:53 +0100

    clean up

--- src/main.c
+++ src/main.c
@@ -14,25 +14,19 @@ pid_t spawn_process(const char *program, char *const argv[]) {
 
   if (pid < 0) {
     perror("fork failed");
-    return -1; // Error
+    return -1; 
   }
 
   if (pid == 0) {
-    // Child: replace with the new program
     execvp(program, argv);
     perror("execvp failed");
-    _exit(1); // Use _exit to avoid flushing parent buffers
+    _exit(1); 
   }
 
-  // Parent: return child's PID
   return pid;
 }
 
 int main(int argc, char *argv[]) {
-  // if (argc < 2) {
-  //   fprintf(stderr, "not enough args");
-  //   return 1;
-  // }
 
   char *args[] = {"./test", NULL};
 
@@ -41,9 +35,6 @@ int main(int argc, char *argv[]) {
   Debugger *dbg = calloc(1, sizeof(Debugger));
 
   attachProc(dbg, pid);
-  // setBreakpointAtMain(dbg);
-  //
-  // readReg(dbg);
 
   shell(dbg);
 }
